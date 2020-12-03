960f004bdceac7cfecbc02fc7b9932d69bc5dd7c
Talita-8 <tlthmf1@outlook.com>
2020-12-03 15:30:16 -0300

    inclusão de filtros

--- src/data.js
+++ src/data.js
@@ -15,3 +15,13 @@ export const filterStatus = function statusFunc(docData, selectStatus){
 export const filterGender = function genderFunc(docData, selectGender){
   return docData.filter(maleFemale => maleFemale.gender === selectGender.value)
 }
+
+export const filterOrder = function orderFunc(docData, selectOrder){
+  
+}
+
+export const filterText = function textFunc(docData, inputText){
+  let qlq = inputText.value.toUpperCase()
+  console.log(qlq)
+  return docData.filter(search => search.name.toUpperCase().startsWith(qlq))
+}
